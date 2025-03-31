67bbb00b2be4caddc8c7a0e456bff38a676cf589
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2025-03-31 09:05:43 +0200

    kiírás új txt fájlba

--- forma1.py
+++ forma1.py
@@ -24,34 +24,29 @@ with open('./beolvasando_adatok/f1.txt', 'r', encoding='utf-8') as forrasfajl:
         f1.append([nev, csapat, gyozelmek, teljesitett_futamok])
 
 # 1. Hány versenyző szerepel a fájlban?
-print(f'A beolvasott fájlban összesen {len(f1)} versenyző szerepel.')
 
 # 2. Melyik versenyző nyerte a legtöbb futamot?
 legjobb_versenyzo = None
 for versenyzo in f1:
     if legjobb_versenyzo is None or versenyzo[2] > legjobb_versenyzo[2]:
         legjobb_versenyzo = versenyzo
-print(f'A legtöbb futamot nyert versenyző: {legjobb_versenyzo[0]}')
 
 # 3. Ki teljesített a legtöbb futamot?
 legtobb_futam = None
 for versenyzo in f1:
     if legtobb_futam is None or versenyzo[3] > legtobb_futam[3]:
         legtobb_futam = versenyzo
-print(f'A legtöbb futamot teljesített versenyző: {legtobb_futam[0]}')
 
 # 4. Átlagosan hány futamot teljesítettek a versenyzők?
 atlagos_futamszam = 0
 for versenyzo in f1:
     atlagos_futamszam += versenyzo[3]
-print(f'Az átlagos futamszám: {atlagos_futamszam / len(f1)}')
 
+# A megoldott feladatokat a kiirt_adatok nevű mappába hozd 
+# létre statisztika.txt néven!
 
-# with open('./kiirt_adatok/statisztika.txt', 'w', encoding='utf-8') as celfajl:
-#         for sor in forrasfajl:
-#             print(sor.strip(), file=celfajl)
-
-# print("A beolvasott fájlban összesen ____ versenyző szerepel.")
-# print("A legtöbb futamot nyert versenyző: ____")
-# print("A legtöbb futamot teljesített versenyző: ____")
-# print("Az átlagos futamszám: ____")
+with open('./kiirt_adatok/statisztika.txt', 'w', encoding='utf-8') as celfajl:
+    print(f'A beolvasott fájlban összesen {len(f1)} versenyző szerepel.', file=celfajl)
+    print(f'A legtöbb futamot nyert versenyző: {legjobb_versenyzo[0]}', file=celfajl)
+    print(f'A legtöbb futamot teljesített versenyző: {legtobb_futam[0]}', file=celfajl)
+    print(f'Az átlagos futamszám: {atlagos_futamszam / len(f1)}', file=celfajl)
